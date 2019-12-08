ff14c70f4bc594a5536bbefc0e5918396a6e4e6e
Edward Martey <martey8709@gmail.com>
2019-12-07 22:06:19 -0600

    The whole enchilada

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # bamazon
+
+Hi there, welcome to Bamazon!
+
+Disclaimer: If you are not familiar with coding terminology and/or programs, Bamazon may not be the bot for you.
+
+Instructions:
+
+1. Open your terminal or command program
+2. Navigate to the file path where the Bamazon app contents are located on your computer
+3. Type in the following command
+  
+  - node bamazonCustomer.js
+    > Will run Bamazon application, listing items available for purchase
+      Ex. - ID# 1 PRODUCT NAME: NBA 2K20 DEPARTMENT: Video Games PRICE: $69.95 QUANTITY AVAILABLE: 150
+
+      ![](/gifs/gif1.gif)
+    
+ - Type quantiy of item desired
+    > Ex. - ? How many NBA 2K20 would you like to buy? 5
+
+      ![](/gifs/gif2.gif)
+
+ - Receipt information will be displayed
+    > Ex. - Your total is:
+            QUANTITY ORDERED: 5 NBA 2K20  at $69.95
+            PRICE:  $349.75
+            TAX @ 0.0875%: $30.6
+            TOTAL BALANCE:  $380.35
+
+     ![](/gifs/gif3.gif)
+
+
+Happy shopping :)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,12 +3,115 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
+
     port: 3306,
 
     user: "root",
 
 
     password: "",
-    database: "bamazon"
+    database: "Bamazon"
 
 });
+
+var cost = 0;
+var total = 0;
+var totalcount = 0;
+
+
+function options() {
+
+
+    connection.query('SELECT * FROM products', function (err, response) {
+        if (err) throw err;
+        console.log("What would you like to buy?");
+
+        for (var i = 0; i < response.length; i++) {
+            console.log("ID# " + response[i].itemid + " PRODUCT NAME: " + response[i].productname + " DEPARTMENT: " + response[i].departmentname + " PRICE: $" + response[i].price + " QUANTITY AVAILABLE: " + response[i].stockquantity);
+        }
+        inquirer.prompt([{
+            type: 'input',
+            name: 'itemid',
+            message: "What is the ID number of the product you would like to buy? [Ctrl + C to exit]"
+
+        }]).then(function (answer) {
+            var itemid = parseInt(answer.itemid)
+
+
+            for (var i = 0; i < response.length; i++) {
+                if (response[i].itemid == answer.itemid) {
+                    var result = response[i];
+                    console.log('We have ' + result.stockquantity + ' ' + result.productname + ' in stock for $' + result.price + ' each');
+
+                    inquirer.prompt([{
+                        type: 'input',
+                        name: 'itemQuantity',
+                        message: 'How many ' + result.productname + ' would you like to buy?'
+
+                    }]).then(function (answer) {
+                        var quantity = parseInt(answer.itemQuantity);
+
+                        if (quantity > result.stockquantity) {
+                            console.log("Sorry we do not have enough available to fullfil your order. Please choose a lower quantity or select another item.");
+                            inquirer.prompt([{
+                                type: 'confirm',
+                                name: 'shop',
+                                message: "Is there anything else we can help you with?"
+
+                            }]).then(function (answer) {
+                                if (answer.shop) {
+                                    options();
+                                } else {
+                                    console.log("Thank you for your business! We hope you come back soon.")
+                                    connection.end();
+                                }
+                            })
+
+                        } else {
+                            console.log("Your total is:");
+
+                            connection.query('UPDATE products SET stockquantity = stockquantity - ? WHERE itemid = ?', [quantity, itemid], function (err, response) {
+                                if (err) throw err;
+                            });
+
+                            var cost = result.price;
+                            var totalPrice = cost * quantity;
+                            var totalPriceRound = Math.round(totalPrice * 100) / 100;
+                            var tax = ((.875 / 10000) * 1000) * totalPrice;
+                            var taxRound = Math.round(tax * 100) / 100;
+                            var total = totalPriceRound + taxRound;
+
+
+
+                            console.log("QUANTITY ORDERED: " + quantity + " " + result.productname + '  at ' + "$" + cost);
+                            console.log("PRICE:  $" + totalPriceRound);
+                            console.log("TAX @ 0.0875%: $" + taxRound);
+                            console.log("TOTAL BALANCE:  $" + total);
+
+                            inquirer.prompt([{
+                                type: 'confirm',
+                                name: 'shop',
+                                message: "Is there anything else you would like to purchase?"
+
+                            }]).then(function (answer) {
+                                if (answer.shop) {
+                                    options();
+                                } else {
+                                    console.log("Thank you for your business.")
+                                    connection.end();
+                                }
+                            })
+
+                        }
+                    })
+                }
+            }
+        })
+
+    });
+    //      
+}
+options();
+
+
+
